84a0f375f8d1559cee8604961eb33ec38d73a864
sborland <sarahborland@eduroam-226-245.ucsc.edu>
2015-02-09 16:27:20 -0800

    shooting and rotation work.

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ function preload() {
 
     game.load.image('background','assets/background.jpg');
     game.load.image('player','assets/rubi.png');
+    game.load.image('player1','assets/rubi.png');
+    game.load.image('bullet','assets/bullet.png');
 
 }
 
 var player;
 var cursors;
+var fireRate = 100;
+var nextFire =0;
 
 function create() {
 
@@ -17,18 +21,32 @@ function create() {
     game.world.setBounds(0, 0, 2000, 1333);
 
     game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
     player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
-
+    //player1 = game.add.sprite(game.world.centerX,game.world.centerY,'player1');
     game.physics.p2.enable(player);
+   // game.physics.arcade.enable(player);
+    
+    player.anchor.set(.5);
+  // player.body.allowRotation = false;
 
+	bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+       bullets.createMultiple(50, 'bullet');
+    bullets.setAll('checkWorldBounds', true);
+    bullets.setAll('outOfBoundsKill', true);
+	
+    
     cursors = game.input.keyboard.createCursorKeys();
-
     game.camera.follow(player);
 
 }
 
 function update() {
+	
+	player.body.rotation = game.physics.arcade.angleToPointer(player) - Math.PI
 
     player.body.setZeroVelocity();
 
@@ -48,6 +66,30 @@ function update() {
     else if (cursors.right.isDown)
     {
         player.body.moveRight(300);
+    }
+    
+ 
+
+    if (game.input.activePointer.isDown)
+    {
+        fire();
+    }
+
+}
+
+function fire() {
+
+    if (game.time.now > nextFire && bullets.countDead() > 0)
+    {
+        nextFire = game.time.now + fireRate;
+
+        var bullet = bullets.getFirstDead();
+
+		var angles = game.physics.arcade.angleToPointer(player) - Math.PI;
+        bullet.reset((player.x+1*Math.cos(-angles)), (player.y+1*Math.sin(-angles)));
+		//bullet.reset(player.x, player.y);
+		
+        game.physics.arcade.moveToPointer(bullet, 500);
     }
 
 }
